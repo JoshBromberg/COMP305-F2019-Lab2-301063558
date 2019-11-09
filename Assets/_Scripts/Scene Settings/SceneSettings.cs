7191a653572b4ea62a5760fa2c61d0c1ac980137
Josh <jbromber@my.centennialcollege.ca>
2019-11-09 14:29:24 -0500

    Added Scene Settings
    Tis a thing

---
+++ Assets/_Scripts/Scene Settings/SceneSettings.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName ="SceneSettings", menuName = "Scene/Settings")]
+[System.Serializable]
+public class SceneSettings : ScriptableObject
+{
+    [Header("Scene Configuration")]
+    public SoundClip activeSoundClip;
+
+    [Header("Scoreboard Labels")]
+    public bool scoreLabelEnabled;
+    public bool livesLabelEnabled;
+    public bool highScoreLabelEnabled;
+
+    [Header("Scene Buttons")]
+    public bool endLabelActive;
+    public bool restartButtonActive;
+    public bool startButtonActive;
+    public bool startLabelActive;
+}
